dca4fe2211a73fcc74c5c9012ef04a7d6a5cf833
Anthony Lopez <tony.loprez.15@gmail.com>
2018-11-29 09:02:58 -0400

    Atualizando o serializer de etapa, setup,procedimentoShort, procedimentoDetails

--- setup/serializers.py
+++ setup/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from setup.models import EtapaProcesso, Setup, Procedimento, OrdemProcesso
 
@@ -11,20 +11,48 @@ class OrdemProcessoSerializer(ModelSerializer):
 
 class EtapaProcessoSerializer(ModelSerializer):
     op = OrdemProcessoSerializer()
+    # gerente = SerializerMethodField()
 
     class Meta:
         model = EtapaProcesso
-        fields = ('id', 'op', 'etapa', 'gerente_desc', 'maquina', 'descricao', 'status')
+        fields = (
+            'id', 'op', 'etapa', 'gerente', 'gerente_desc',
+            'maquina', 'descricao', 'status'
+        )
 
 
 class SetupSerializer(ModelSerializer):
+    tipo = SerializerMethodField()
 
     class Meta:
         model = Setup
         fields = '__all__'
 
+    def get_tipo(self, obj):
+        return obj.get_tipo_display()
 
-class ProcedimentoSerializer(ModelSerializer):
+
+#  Mostra os campos necessários quando for listar uma coleção de procedimentos
+class ProcedimentoShortSerializer(ModelSerializer):
+    status = SerializerMethodField()
+
+    class Meta:
+        model = Procedimento
+        fields = (
+            'id', 'ordem_roteiro', 'tempo_estimado', 'descricao',
+            'setup', 'status'
+        )
+
+    def get_status(self, obj):
+        return obj.get_status_display()
+
+
+class ProcedimentoDetailsSerializer(ModelSerializer):
+    status = SerializerMethodField()
+
     class Meta:
         model = Procedimento
         fields = '__all__'
+
+    def get_status(self, obj):
+        return obj.get_status_display()
