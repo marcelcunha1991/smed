369f2154acfed109c3278871ea97d57bb7530ed8
Anthony Lopez <tony.lopez.15@gmail.com>
2019-02-07 09:45:22 -0400

    Retornando uma lista de gerentes

--- accounts/viewsets.py
+++ accounts/viewsets.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate
 from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
@@ -37,6 +38,12 @@ class UserViewSet(ModelViewSet):
             return Response(serializer.data)
         except Exception as e:
             return Response({'mensage': e.args[0]}, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(methods=['get'], detail=False)
+    def gerentes(self, request):
+        gerentes = User.objects.filter(cargo__descricao='Gerente')
+        serializer = UserLoggedSerializer(gerentes, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class LoginViewSet(APIView):
